bb2b8596c42387eb4b85980c443dc4ab1ac12d81
rkausin <Ryan@Tests-MacBook-Pro.local>
2017-03-13 00:14:26 -0700

    made weights 2d array & computed errors

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main
 {
         /// Prints the commandline instructions.
-        public static void main(String[] args)
+        public static void main(String[] args) throws IOException
         {
+            
+            FileReader fr = new FileReader("file2.txt");
+            BufferedReader br = new BufferedReader(fr);
+            
             
             bp();
             
@@ -10,18 +14,19 @@ public class Main
             System.out.print(sigmoid(2));
         }
     
-    public static void bp() { // backpropagation function
+    public static void bp() throws IOException{ // backpropagation function
         
         //File file = new File("file.txt");
         //BufferedReader reader = null;
         
-        double w1[] = new double[46080]; // #input * #hidden nodes
-        double w2[] = new double[6]; // #hidden nodes * #finals nodes (2)
         
-        double z1[] = new double[3]; // #hidden nodes
+        double w1[][] = new double[3][15361]; // #input * #hidden nodes
+        double w2[][] = new double[2][4]; // #hidden nodes * #finals nodes (2)
+        
+        double z1[] = new double[4]; // #hidden nodes
         double z2[] = new double[2]; // #final nodes
         
-        double a1[] = new double[3]; // #hidden nodes
+        double a1[] = new double[4]; // #hidden nodes
         double a2[] = new double[2]; // #final nodes
         
         double l1[] = new double[3]; // learning error for hidden layer
@@ -31,17 +36,25 @@ public class Main
         
         int numTrain = 100;
         for (int i = 0; i < 100; ++i){ // for each training image
-            for (int j = 0; j < w1.length; ++j)
-                z1 [j % 3] += w1[j]* (VALUE IN X1);
-                        // set a1 to xk, each value in a1 is a sum of all x values, multiplied by the weights leading to that hidden node
-                
-            for (int h = 0; h < z1.length; ++h){ // compute a(l) for all layers l
-                a1 [h] = sigmoid(z1[h]); // apply sigmoid function to z1
+            
+            for(int j = 0; j < w1.length; j++){
+                for(int x = 0; x < w1[0].length - 1; x++){
+                    z1[j] += w1[j][x]  * input[i][x];
+                }
+                z1[j] += w1[j][15360]; //add a0 bias
             }
             
-            for (int m = 0; m < w2.length; ++m)
-                z2 [m % 2] += w2[j]* a1[m % 3]; // uses weights in second layer to produce z2
+            for (int j = 0; j < z1.length - 1; j++){ // compute a(l) for all layers l
+                a1[j] = sigmoid(z1[j]); // apply sigmoid function to z1
+            }
+            a1[3] = .5; // initialize a1 bias constant in hidden layer ???CHECK????
             
+            for(int j = 0; j < w2.length; j++){
+                for(int x = 0; x < w2[0].length; x++){
+                    z2[j] += w2[j][x] * a1[x];  // uses weights in second layer to produce z2
+                }
+            }
+
             for (int k = 0; k < z2.length; ++k){
                 a2[k] = sigmoid(z2[k]);
             }
@@ -50,24 +63,26 @@ public class Main
             l2[1] = a2[1] - y[i]; // compute error in final layer
             
             
-            for (int n = 0; n < 3; ++n) {
-                for (int o = 0; o < 6; ++o){
-                    l1[n] *= w2[o/2] * l2[n % 2] * a1[n] * (1 - a[n]); // compute error for hidden layer
+            for(int q = 0; q < l1.length; q++){                     //compute error for hidden layer
+                l1[q] = (w2[0][q]*l2[0] + w2[1][q]*l2[1]) * a1[q] * (1-a1[q]);
+            }
+            
+            for(int j = 0; j < w1.length; j++){
+                for(int x = 0; x < w1[0].length; x++){
+                    w1[j][x] -= learnRate * (a1[j] * l1[j]);
                 }
             }
             
-            for (int p = 0; p < w1.length; ++p){ // update weights for w1
-                w1[p] -= learnRate * (a1[p % 3] * l1[p % 3]);
-            }
-            
-            for (int q = 0; q < w2.length; ++q){ // update weights for w2
-                w2[q] -= learnRate * (a2[q % 2] * l2[q % 2])
+            for(int j = 0; j < w2.length; j++){
+                for(int x = 0; x < w2[0].length; x++){
+                    w2[j][x] -= learnRate * (a2[j] * l2[j]);
+                }
             }
             
                         // compute error for all hidden layers
                         // updates weights using heuristic optimization
                         // w = w - (learning rate)*(a(l-1)* lambda(l))
-        }
+        }//end training loop
         
     }
     
@@ -76,5 +91,3 @@ public class Main
         return 1/(1 + Math.pow(2.7182818285, z));
     }
 }
-
-
